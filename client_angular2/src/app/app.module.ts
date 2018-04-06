6128428fb10a37c3a3883bc06521f533f0fb2636
YifengZhong <yf_zhong@hotmail.com>
2018-04-06 01:00:06 -0700

    upload

---
+++ client_angular2/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {StompService} from 'ng2-stomp-service';
+// use ReactiveFormsModule Only when using Template Driven Forms
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { ConnectService } from "./service/connectionSrv";
+import { ConnectComponentComponent } from './connect-component/connect-component.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ConnectComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    ReactiveFormsModule
+  ],
+  providers: [ConnectService,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client_angular2/src/app/service/connectionSrv.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import {Subject} from 'rxjs/Subject';
+import {Observable} from 'rxjs/Observable';
+import 'stompjs';
+declare let Stomp:any;
+
+@Injectable()
+export class ConnectService {
+    private _stompClient;
+    private _stompSubject : Subject<any> = new Subject<any>();
+    public  delay(ms: number) {
+        return new Promise( resolve => setTimeout(resolve, ms) );
+    }
+    public login(_name:string) {
+        console.log(_name);
+        this._stompClient.send("/app/login", {}, JSON.stringify({'fullName': _name}));
+
+    }
+    public connect(_name: string):Observable<any> {
+        let self = this;
+        let webSocket = new WebSocket('ws://localhost:8080/gs-guide-websocket/websocket');
+        this._stompClient = Stomp.over(webSocket);
+        this._stompClient.connect({}, function (frame) {
+
+            self._stompClient.subscribe('/topic/messaging', function (stompResponse) {
+                // stompResponse = {command, headers, body with JSON 
+                // reflecting the object returned by Spring framework}
+                self._stompSubject.next(JSON.parse(stompResponse.body));
+                
+            });
+            self._stompClient.subscribe('/topic/login', function (stompResponse) {
+                // stompResponse = {command, headers, body with JSON 
+                // reflecting the object returned by Spring framework}
+                console.log("get the feedback",JSON.parse(stompResponse.body));
+                self._stompSubject.next(JSON.parse(stompResponse.body));
+                //self._stompSubject.next(JSON.parse(stompResponse.body));
+                
+            });
+            
+        },function(err) {
+            self._stompSubject.next(1);
+        });
+        return self._stompSubject;
+    }
+    public disConnect(username:string) {
+        console.log("ccc");
+        this._stompClient.send("/app/logout", {}, JSON.stringify({'fullName': username}))
+        if (this._stompClient !== null) {
+            this._stompClient.disconnect();
+          }
+      
+    }
+
+    public send(username:string, _payload: string,toUser:string) {
+        //TODO get name
+        console.log("abc",toUser);
+        this._stompClient.send("/app/messaging", {}, JSON.stringify({'name': username,'content':_payload,'toUser':toUser}));
+    }
+
+    public getObservable() : Observable<any> {
+        return this._stompSubject.asObservable();
+    }
+     
+}
